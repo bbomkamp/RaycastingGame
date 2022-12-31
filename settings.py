56e746fa20e9f1649577487937ef17971ee8f025
bbomkamp <bradbomkamp@gmail.com>
2022-12-31 16:00:31 -0600

    raycasting done

--- settings.py
+++ settings.py
@@ -1,6 +1,10 @@
+import math
+
 # Game Setting
 RES = WIDTH, HEIGHT = 1600, 900
 FPS = 60
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
 
 
 # Player Settings
@@ -8,3 +12,16 @@ PLAYER_POS = 1.5 , 5 # mini_map
 PLAYER_ANGLE = 0
 PLAYER_SPEED = 0.004
 PLAYER_ROT_SPEED = 0.002
+
+
+# RayCasting Settings
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 20
+
+# 3D Setting
+SCREEN_DIST = HALF_WIDTH / math.tan(HALF_FOV)
+SCALE = WIDTH // NUM_RAYS
